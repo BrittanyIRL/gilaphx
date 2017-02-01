37f8c18f82d5a041e27dd7ddccfe504afca304ac
Brittany Brassell <brittany.brassell@gmail.com>
2017-01-31 19:43:07 -0700

    fullpage js working with homepage, nav behaving properly

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,29 @@
+//
+// Jekyll - Javascript
+// Author: @brittanyIRL
+//
+
 $( document ).ready(function() {
+
+
+  // control nav visibility?  //
+  // ************************ //
+
+  // $(window).scroll(function() {
+  //   if (scroll >= 50) {
+  //       $('#logo-image').attr('src', 'img/Acme_Monogram_Colour.png')
+  //       $(".important-class").addClass("padding-on-my-header");
+  //   }
+  //   if (scroll < 50) {
+  //       $(".important-class").removeClass("padding-on-my-header");
+  //       $('#logo-image').attr('src', 'img/Acme_Colour.png')
+  //   }
+  // });
+
+
+  // Materialize Plugins       //
+  // ************************ //
   // Initialize collapse button
-  
   $('.button-collapse').sideNav({
       menuWidth: 135, // Default is 300
       edge: 'left', // Choose the horizontal origin
@@ -10,29 +33,25 @@ $( document ).ready(function() {
   $('.collapsible').collapsible();
   $('.scrollspy').scrollSpy();
 
+  // fullpage.js for homepage //
+  // ************************ //
+
   // initialize fullpage js
-  $('#homepage').fullpage({
-  	//Navigation
-        menu: '#menu',
-        lockAnchors: true,
-        anchors:['firstPage', 'secondPage'],
+	$('#fullpage').fullpage({
+		//Navigation
         navigation: false,
-        navigationPosition: 'right',
-        navigationTooltips: ['firstSlide', 'secondSlide'],
         showActiveTooltip: false,
         slidesNavigation: false,
-        slidesNavPosition: 'bottom',
-
         //Scrolling
         css3: true,
         scrollingSpeed: 700,
-        autoScrolling: false,
-        fitToSection: true,
+        autoScrolling: true,
+        fitToSection: false,
         fitToSectionDelay: 1000,
         scrollBar: false,
         easing: 'easeInOutCubic',
         easingcss3: 'ease',
-        loopBottom: true,
+        loopBottom: false,
         loopTop: false,
         loopHorizontal: true,
         continuousVertical: false,
@@ -44,7 +63,7 @@ $( document ).ready(function() {
         resetSliders: false,
         fadingEffect: false,
         normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
+        scrollOverflow: true,
         scrollOverflowReset: false,
         scrollOverflowOptions: null,
         touchSensitivity: 15,
@@ -54,17 +73,14 @@ $( document ).ready(function() {
         //Accessibility
         keyboardScrolling: true,
         animateAnchor: true,
-        recordHistory: true,
+        recordHistory: false,
 
         //Design
-        controlArrows: true,
+        controlArrows: false,
         verticalCentered: true,
-        sectionsColor : ['#ccc', '#fff'],
-        paddingTop: '3em',
-        paddingBottom: '10px',
-        fixedElements: '#header, .footer',
+        fixedElements: '#slide-out',
         responsiveWidth: 0,
-        responsiveHeight: 0,
+        responsiveHeight: '100vh',
         responsiveSlides: false,
 
         //Custom selectors
@@ -74,17 +90,36 @@ $( document ).ready(function() {
         lazyLoading: true,
 
         //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
+        onLeave: function(index, nextIndex, direction){
+        },
+        afterLoad: function(anchorLink, index){
+        	console.log("afterLoad ", anchorLink, index);
+        	showOrHideSideNav(index);
+        },
         afterRender: function(){},
         afterResize: function(){},
         afterResponsive: function(isResponsive){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-    });
-
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+        },
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+        }
+	});
+	
   // check if first two home screens are visible, if so - hide nav
 
+  	function showOrHideSideNav(section_index){
+  		console.log(section_index);
+  		if(section_index <= 2){ // starts at 1 not 0
+  			console.log("found gila intro");
+  			$("#navigation_image").css("display", "none");
+  		} else if (section_index == 4){
+  			// would be cool to make it white here...
+  			$("#navigation_image").css("display", "none");
+  		}
+  		else {
+  			$("#navigation_image").css("display", "block");
+  		}
+  	}
   // if($(".intro_splash:visible")){
 	 //  	console.log("visible");
 	 //  }
