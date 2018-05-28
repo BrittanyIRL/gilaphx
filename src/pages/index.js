9ef87004508c5446ef72050ece8632aa4a800066
Brittany Brassell <brittany.brassell@gmail.com>
2018-05-28 15:00:53 -0700

    gutting fullpage for react, can't use it multiple times and honestly 100vw + 100vh more than does the trick

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,11 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Head from '../components/hoc/Head/Head';
 
-// fullpage
-import {SectionsContainer, Section} from 'react-fullpage';
 // page views
 import LandingView from '../components/landing/landing-view';
 import IntroView from '../components/landing/intro-view';
 
-let fullpage_options = {
-  arrowNavigation : true,
-  delay : 500,
-  scrollingSpeed : 400,
- sectionClassName : 'section',
- scrollBar : true,
- navigation : true,
- verticalAlign : false,
- recordHistory : false
-};
+import defaultStyles from '../components/default.module.css';
 
 let homeDescription = "<p>Gila. Seen afar in a Baltimore dream. An eastern province bordering our fair city. A tributary cutting through its heart. A dragon roaming the desert sunset. A people whose culture outlives us all.</p> <p>Gila. Pronounced \ǵē-lə\. Like so much of our modernity, an intentional misplacement. Appropriation disguised as ignorance. A wrong as a reminder. To learn and unlearn.</p> <p>Gila. Nomads preserving the culture of the native tongue. Art activism through radical collectivism. Bridges built in and out of knowledge. Eternal joy. </p>";
 
@@ -41,13 +30,12 @@ class RootIndex extends React.Component {
     return (
       <div>
         <Head title="Gila PHX" description={homeDescription} />
-
-        <SectionsContainer {...fullpage_options}>
-          <Section><LandingView onClick={this.hereRedirect} /></Section>
-          <Section>
+          <div className={defaultStyles.fullPageWrapper}>
+            <LandingView onClick={this.hereRedirect} />
+          </div>
+          <div className={defaultStyles.fullPageWrapper}>
             <IntroView />
-          </Section>
-        </SectionsContainer>
+          </div>
       </div>
     )
   };
